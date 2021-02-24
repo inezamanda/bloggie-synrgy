50e4b0aaa2d2f0912edd0acddcddac049a4eb9c4
Chaniaeva <chania.eva@gmail.com>
2021-02-24 11:30:35 +0700

    first initiation

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.static('views'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
